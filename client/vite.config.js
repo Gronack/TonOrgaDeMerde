ecbf66dc0cc67abc2c19bedd208c9a37ae999432
Gronack <universkilleur@outlook.fr>
2025-05-17 21:04:28 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-});
+export default {
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://tonorgademerde.onrender.com')
+  }
+};
