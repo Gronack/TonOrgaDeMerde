abce8c7e77fd1e75746b8aef23fda2f36a0cf58d
Gronack <universkilleur@outlook.fr>
2025-05-17 20:35:26 +0200

    Update userService.js

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,11 +1,8 @@
 export async function fetchUser() {
-  const res = await fetch("http://localhost:3001/auth/user", {
-    credentials: "include", // ⚠️ obligatoire
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/user`, {
+    credentials: "include" // ✅ indispensable
   });
 
-  if (!res.ok) {
-    throw new Error("Utilisateur non connecté");
-  }
-
+  if (!res.ok) throw new Error("Utilisateur non connecté");
   return res.json();
 }
